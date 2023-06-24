4bb73e7e8b9693c5f620653f4fa0e5379902790b
JohnSmith <zhengmiao2000@outlook.com>
2023-06-24 15:39:48 +0800

    - 完成了读取jiapu.txt的代码，并通过输出文件进行了验证

--- plot_family_tree.py
+++ plot_family_tree.py
@@ -3,14 +3,19 @@ import matplotlib
 import numpy as np
 
 class Person:
-    """管理每个人信息的类，包括姓名、世代、后代"""
+    """管理每个人信息的类，包括姓名、世代、后代、个人编号"""
     
-    def __init__(self, name, generation):
+    def __init__(self, name, generation = -1, id = ""):
         self.name = name
         self.generation = generation
-        self.children = []
-        self.max_child_number = 1  # 代表人数最多的一辈有几个人
+        self.ID = id # 个人身份编号
         
+        self.children = [] # 存储子代person对象的列表
+        self.max_child_number = 1  # 代表人数最多的一辈有几个人
+    
+    def add_ID(self, id):
+        self.ID = id
+            
     def add_child(self, child):
         """添加自己的直系后代"""
         self.children.append(child)
@@ -23,29 +28,62 @@ class FamilyTree:
     
     def __init__(self, root):
         self.root = root
+        self.dict = {}
         
         self.x_length = 0.2  # 横线单位长度
         self.y_height = 2.5  # 竖线长度
-        self.y_word = 0.5  # 人名中每个字中心的竖向距离
-        self.y1 = self.y_word / 5
-        self.y2 = 0.4 #
+        self.y_word = 0.8  # 人名中每个字中心的竖向距离
+        self.y1 = 0.1 # 子代名字离下方竖线距离微调，增大该值则距离减小
+        self.y2 = 0.7 # 子代名字离上方竖线距离微调，增大该值则距离增大
+        
+        # 先将root加入到字典中
+        if root.ID:
+            self.dict[root.ID] = root 
         
     def add_child(self, parent, child):
         """添加一个人的直系后代"""
         parent.children.append(child)
-        
-    def read_family_tree(self):
-        """从已有文件中读取家谱信息"""
-        
-        
+                     
     def plot_tree(self):
         """绘制族谱"""
-        self.read_family_tree()
+        self._read_family_tree()
         
         fig, ax = plt.subplots()
         self._plot_person(ax, self.root)
         plt.axis('off')
         plt.show()
+    
+    def _read_family_tree(self):
+        """从已有文件中读取家谱信息"""
+        with open('jiapu.txt', 'r') as f:
+            next(f) # 跳过前两行
+            next(f)
+            for line in f:
+                # 对字符处理的准备工作
+                line = line.rstrip('\n') # 去掉行尾的换行符
+                strings = line.split('\t') # 分割字符串
+                # 去掉每个字符串中的双引号
+                for i, s in enumerate(strings):
+                    new_string = s.replace('"', '')
+                    strings[i] = new_string
+                
+                # 正式开工
+                if self.dict.get(strings[4], 0): # 如果字典中可以查到这个长辈
+                    child = Person(strings[2], int(strings[3]), strings[5]) # 创建这个子代的实例
+                    self.dict[strings[4]].add_child(child) # 添加到长辈的子代列表中
+                    self.dict[strings[5]] = child # 将该子代添加到字典中
+                else:
+                    print("该行输入错误：{}".format(line))
+        
+        # 可以通过下面的输出文件，验证读取文件的正确
+        # with open('test.txt', 'w') as o:
+        #     for key, value in self.dict.items():
+        #         parent_name = value.name
+        #         children = value.children
+        #         o.write(f"{parent_name} :")
+        #         for child in children:
+        #             o.write(f" {child.name}")
+        #         o.write("\n")
         
     def _plot_person(self, ax, person, x=0, y=0):
         """绘制族谱中每个人的部分，递归调用"""
@@ -114,14 +152,27 @@ class FamilyTree:
 # 设置中文字体
 matplotlib.rcParams['font.family'] = ['SimHei']
 
-root = Person('思温', 1)
-child1 = Person('仕忠', 2)
-child2 = Person('仕贤', 2)
-grandchild1 = Person('敏', 3)
+debug = 0
 
-family_tree = FamilyTree(root)
-family_tree.add_child(root, child1)
-family_tree.add_child(root, child2)
-family_tree.add_child(child1, grandchild1)
+if debug:
+    root = Person('思温', 1)
+    child1 = Person('仕忠', 2)
+    child2 = Person('仕贤', 2)
+    grandchild1 = Person('敏', 3)
+    ggrandchild1 = Person('彪', 4)
+    ggrandchild2 = Person('宪', 4)
+    ggrandchild3 = Person('常', 4)
+
+    grandchild1.add_child(ggrandchild1)
+    grandchild1.add_child(ggrandchild2)
+    grandchild1.add_child(ggrandchild3)
+
+    family_tree = FamilyTree(root)
+    family_tree.add_child(root, child1)
+    family_tree.add_child(root, child2)
+    family_tree.add_child(child1, grandchild1)
+else:
+    root = Person('思温', 1, 'S0010001')
+    family_tree = FamilyTree(root)
 
 family_tree.plot_tree()
